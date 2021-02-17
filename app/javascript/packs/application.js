31dc73b3758cacc3abf105ec3f80fbe19dd5260a
rafhernandez <rafnhernandez@gmail.com>
2021-02-17 11:57:13 +0800

    Fix Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,14 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import 'bootstrap';
+import '../stylesheets/application'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+$(function () {
+    console.log('Hello World from Webpacker');
+});
